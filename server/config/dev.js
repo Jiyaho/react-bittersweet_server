596a9ebe6c0f69eb3bea5e9925dc28e6ac6b7c77
Jiho Shin <masqu2n@gmail.com>
2022-12-05 17:44:47 +0900

    clear git cache

--- server/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://jiyaho:12345@react-bittersweet.hsm2id0.mongodb.net/?retryWrites=true&w=majority",
-};
-
-// a. config 폴더 > dev.js (개발환경에서 설정할 것)
-// b. config 폴더 > prod.js (배포환경에서 설정할 것)
-// c. config 폴더 > key.js (환경변수가 개발환경인지 배포환경인지에 따라 실행시킬 if문 삽입)
